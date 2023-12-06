b2d6a3cf60bdbd45bb77b8fe09dc782fa797555a
Andris Ševčenko <andris@sevcenko.net>
2023-12-06 09:27:14 +0200

    Drop progress info.

--- 6/part-1/do.php
+++ 6/part-1/do.php
@@ -12,16 +12,11 @@ for ($race = 0, $raceMax = count($times); $race < $raceMax; $race++) {
     $raceTime = $times[$race];
     $raceDist = $dists[$race];
 
-    echo sprintf("Race distance %d, record %d\n", $raceDist, $raceTime);
-
     $waysToWin = 0;
     for ($hold = 1; $hold < $raceTime; $hold++) {
-        if ($hold % 1000000 == 0) {
-            echo $hold."\n";
-        }
+
         $distance = $hold * ($raceTime - $hold);
         if ($distance > $raceDist) {
-//            echo sprintf("Win by holding for %d, speed is %d, total distance is %d\n", $hold, $hold, $distance);
             $waysToWin++;
         }
     }
